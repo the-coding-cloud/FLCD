3e7fcd866751a9588dbb2d43bd8d2560cbd8efc3
haloandrei <haloandrei@gmail.com>
2020-11-24 15:48:10 +0100

    added anotherTry, with input stack poping nicely

--- Lab5/Parser.py
+++ Lab5/Parser.py
@@ -1,6 +1,8 @@
 from Lab5.Grammar import Grammar
 
 class ParserRecursiveDescend:
+    #input must be reversed for the pop to work
+    # such that the expansion of the work stack to be ___appended___ in the input list
     def __init__(self):
         self.grammar = Grammar("g1.txt")
         self.work = []
@@ -28,4 +30,21 @@ class ParserRecursiveDescend:
     def success(self):
         self.state = "f"
         self.index += 1
-    
+
+    def anotherTry(self):
+        if self.index == 1 and self.work[len(self.work)-1][0] == self.grammar.getStartSymbol():
+            raise Exception("ERROR")
+        currentNonTerm,nonTermIndex = self.work.pop()
+        currentNonTermProductions = self.grammar.printProductionsForNonTerminal(currentNonTerm)
+        if len(currentNonTermProductions)-1 > nonTermIndex:
+            self.state = "q"
+            self.work.append((currentNonTerm,nonTermIndex+1))
+            #self.input.pop()  #handle this better, just one terminal will pop, not the whole non terminal expansion
+            for i in currentNonTermProductions[nonTermIndex]:
+                self.input.pop()
+            self.input.append(currentNonTermProductions[nonTermIndex])
+        else:
+            for i in currentNonTermProductions[nonTermIndex]:
+                self.input.pop()
+            self.input.append(currentNonTerm)
+
